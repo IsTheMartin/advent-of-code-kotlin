121a6006abb1680dff8ef79f69bcb513754c43a7
Martin Morales <themartin93@gmail.com>
2024-01-07 21:16:56 -0600

    Some clean code in day 4

--- src/year2023/Day04.kt
+++ src/year2023/Day04.kt
@@ -5,13 +5,8 @@ import kotlin.math.pow
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map { line ->
-            val winningNumbers = line.substringAfter(":")
-                .substringBefore("|")
-                .split(" ")
-                .filter { it.isNotEmpty() || it == " " }
-            val myNumbers = line.substringAfter("|")
-                .split(" ")
-                .filter { it.isNotEmpty() || it == " " }
+            val winningNumbers = getWinningNumbers(line)
+            val myNumbers = getMyNumbers(line)
             winningNumbers.intersect(myNumbers).size
         }.sumOf {
             2.0.pow(it - 1).toInt()
@@ -20,20 +15,14 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         val cardMatches = input.map { line ->
-            val winningNumbers = line.substringAfter(":")
-                .substringBefore("|")
-                .split(" ")
-                .filter { it.isNotEmpty() || it == " " }
-            val myNumbers = line.substringAfter("|")
-                .split(" ")
-                .filter { it.isNotEmpty() || it == " " }
+            val winningNumbers = getWinningNumbers(line)
+            val myNumbers = getMyNumbers(line)
             winningNumbers.intersect(myNumbers).size
         }
 
         val cards = IntArray(cardMatches.size) { 1 }
         cardMatches.forEachIndexed { index, matches ->
             repeat(matches) {
-                val pos = index + it + 1
                 cards[index + it + 1] += cards[index]
             }
         }
@@ -51,9 +40,17 @@ fun main() {
     part2(input).println()
 }
 
-
-fun getNumbersInCard(input: String): List<String> {
-    return input.trim()
-        .replace("\\s+".toRegex(), " ")
+fun getWinningNumbers(line: String): Set<String> {
+    return line.substringAfter(":")
+        .substringBefore("|")
         .split(" ")
+        .filter { it.isNotEmpty() || it == " " }
+        .toSet()
+}
+
+fun getMyNumbers(line: String): Set<String> {
+    return line.substringAfter("|")
+        .split(" ")
+        .filter { it.isNotEmpty() || it == " " }
+        .toSet()
 }
